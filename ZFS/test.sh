a0f8bb2cdddfee4909aff5c58c42b52072356380
xanthocercis <92279348+xanthocercis@users.noreply.github.com>
2025-02-09 15:11:39 +0400

    Add files via upload

---
+++ ZFS/test.sh
@@ -0,0 +1,58 @@
+#!/bin/bash
+
+ cat << EOF > /etc/yum.repos.d/CentOS-Base.repo
+[base]
+name=CentOS-7 - Base
+baseurl=https://vault.centos.org/7.9.2009/os/x86_64/
+gpgcheck=1
+gpgkey=file:///etc/pki/rpm-gpg/RPM-GPG-KEY-CentOS-7
+enabled=1
+
+[updates]
+name=CentOS-7 - Updates
+baseurl=https://vault.centos.org/7.9.2009/updates/x86_64/
+gpgcheck=1
+gpgkey=file:///etc/pki/rpm-gpg/RPM-GPG-KEY-CentOS-7
+enabled=1
+
+[extras]
+name=CentOS-7 - Extras
+baseurl=https://vault.centos.org/7.9.2009/extras/x86_64/
+gpgcheck=1
+gpgkey=file:///etc/pki/rpm-gpg/RPM-GPG-KEY-CentOS-7
+enabled=1
+EOF
+
+    # Обновляем кэш yum
+    yum clean all
+    yum makecache
+
+	# Configure DNS using nmcli
+        connection_name=$(nmcli -t -f NAME connection show --active | head -n 1)
+        if [ -n "$connection_name" ]; then
+          nmcli con mod "$connection_name" ipv4.dns "8.8.8.8 1.1.1.1" ipv4.ignore-auto-dns yes
+          nmcli con up "$connection_name"
+        fi
+          #install zfs repo
+          yum install -y http://download.zfsonlinux.org/epel/zfs-release.el7_8.noarch.rpm
+          #import gpg key 
+          rpm --import /etc/pki/rpm-gpg/RPM-GPG-KEY-zfsonlinux
+          #install DKMS style packages for correct work ZFS
+          yum install -y epel-release kernel-devel zfs
+          #change ZFS repo
+          yum-config-manager --disable zfs
+          yum-config-manager --enable zfs-kmod
+          yum install -y zfs
+          #Add kernel module zfs
+          modprobe zfs
+          #install wget
+          yum install -y wget
+
+zpool create otus1 mirror /dev/sdb /dev/sdc
+zpool create otus2 mirror /dev/sdd /dev/sde
+zpool create otus3 mirror /dev/sdf /dev/sdg
+zpool create otus4 mirror /dev/sdh /dev/sdi
+zfs set compression=lzjb otus1
+zfs set compression=lz4 otus2
+zfs set compression=gzip-9 otus3
+zfs set compression=zle otus4
